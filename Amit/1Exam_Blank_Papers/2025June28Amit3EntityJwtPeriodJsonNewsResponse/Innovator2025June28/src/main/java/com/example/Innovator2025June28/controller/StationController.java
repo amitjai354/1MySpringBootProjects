6b87ef4b8932084696f2a5a582ec28bbdbd6794e
ankit <ankitjai3@gmail.com>
2025-06-30 11:34:09 +0530

    status cudes

--- Amit/1Exam_Blank_Papers/2025June28Amit3EntityJwtPeriodJsonNewsResponse/Innovator2025June28/src/main/java/com/example/Innovator2025June28/controller/StationController.java
+++ Amit/1Exam_Blank_Papers/2025June28Amit3EntityJwtPeriodJsonNewsResponse/Innovator2025June28/src/main/java/com/example/Innovator2025June28/controller/StationController.java
@@ -10,6 +10,25 @@ import com.example.Innovator2025June28.entity.Station;
 public class StationController {
 	
 	
+//in security config in exam , I have written these
+//Red api by "ADMIN", Green by "USER", Blue by anyone without requiring authentication, JWT Token
+//Blue api: /signUp, /login, /station/list, /show/list
+//Green: /show/get/airing, /show/popularShow
+//Red: /station/add, /station/update, show/add
+
+//Status code: check in test case for each api
+// /signUp : 201 and nothing given in exam
+// /login : 200, 400 Invalid Crdentials!, 
+// /station/list : 200, in test case unauthrized and Forbidden but that handled from security config
+// /show/list : 200 and nothing given in exam, in test case unauthrized and Forbidden but that handled from security config
+// /show/get/airing : 200 400 if no data, 
+// /show/popularShow : 200 and nothing given, in test case unauthrized and Forbidden but that handled from security config
+// /station/add : those who have role ADMIN and operator id point to the owner who created station details 201
+// /station/update : authenticated user have role ADMIN and is creator of id object, 200, Forbidden if not the creator, 400 if no data found
+// /show/add : 201 400 is any validation issue like missing required fields or invalid station id
+
+	
+	
 	//Error: JWT signature does not match locally computed signature. JWT validity cannot be asserted and should not be trusted
 	@PostMapping("/station/add")
 	public ResponseEntity<Object> postData(Station station){
